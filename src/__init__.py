a31f50e80fb8f78cc1d45a97dbff7e421f5b8c78
archit03 <architsood99@hotmail.com>
2025-01-23 16:29:41 +0530

    Add comprehensive training and dataset configuration to YAML files

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,8 @@ from .tokenizer import MedicalTokenizer
 from .dataset import DatasetProcessor
 from .model import Transformer
 from .training import Trainer
-from .evaluation import Evaluate_Model
-from inference import generate_text
+from .evaluation import evaluate_model
+from .inference import generate_text
 from .utils import setup_logger, MemoryMonitor
 
 __version__ = "0.1.0"
@@ -11,11 +11,10 @@ __version__ = "0.1.0"
 __all__ = [
     "MedicalTokenizer",
     "DatasetProcessor",
-    "TransformerModel",
-    "train_one_epoch",
+    "Transformer",
+    "Trainer",
     "evaluate_model",
     "generate_text",
-    "setup_logging",
+    "setup_logger",
     "MemoryMonitor"
 ]
-
